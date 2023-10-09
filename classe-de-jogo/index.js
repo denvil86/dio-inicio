5c16e0f06b2b270a9b2ae6851d192be7ae4661bc
denvil86 <denisvilante@gmail.com>
2023-10-08 22:32:30 -0300

    enviando arquivos

---
+++ classe-de-jogo/index.js
@@ -0,0 +1,27 @@
+class hero {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+    atacar() {
+        let ataque = '';
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'magia';
+                break;
+            case 'guerreiro':
+                ataque = 'espada';
+                break;
+            case 'monge':
+                ataque = 'artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'shuriken';
+                break;
+        };
+
+        return (`O ${this.tipo} atacou usando ${ataque} `);
+    }
+}
+
